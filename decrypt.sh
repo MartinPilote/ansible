6862535c40d2571da86ac2fce4adfae8e87f5d10
Martin Pilote <martin@mpilote.com>
2023-07-01 09:38:35 -0400

    Add servers

---
+++ decrypt.sh
@@ -0,0 +1,6 @@
+ansible-vault decrypt ./host_vars/srv-dc02/vault.yml
+ansible-vault decrypt ./host_vars/srv-dc01/vault.yml
+ansible-vault decrypt ./host_vars/srv-graph/vault.yml
+ansible-vault decrypt ./host_vars/srv-log/vault.yml
+ansible-vault decrypt ./host_vars/srv-metric/vault.yml
+ansible-vault decrypt ./host_vars/srv-postgresql/vault.yml
---
+++ encrypt.sh
@@ -0,0 +1,6 @@
+ansible-vault encrypt ./host_vars/srv-dc02/vault.yml
+ansible-vault encrypt ./host_vars/srv-dc01/vault.yml
+ansible-vault encrypt ./host_vars/srv-graph/vault.yml
+ansible-vault encrypt ./host_vars/srv-log/vault.yml
+ansible-vault encrypt ./host_vars/srv-metric/vault.yml
+ansible-vault encrypt ./host_vars/srv-postgresql/vault.yml
